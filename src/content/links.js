d2a2ff718e9ee8a3dd0e4816ad69d18c786bd39d
dchima <dcnnad@gmail.com>
2019-12-13 22:27:35 +0100

    setup react starter packages and files

---
+++ src/content/links.js
@@ -0,0 +1,13 @@
+const links = {
+  navLinks: [
+    {
+      name: 'browse',
+      url: '/browse',
+    },
+    {
+      name: 'contribute',
+      url: '/contribute',
+    },
+  ],
+};
+export default links;
