32cc3c16305eab488996cda133c07c3a87561601
dchima <dcnnad@gmail.com>
2020-01-13 01:44:10 +0100

    removed redux developer tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+// for production
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -8,10 +9,26 @@ const middlewares = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
   applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
 );
 
 export default store;
+
+// for development
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers';
+
+// const initialState = {};
+// const middlewares = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//   applyMiddleware(...middlewares),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//   ),
+// );
+
+// export default store;
